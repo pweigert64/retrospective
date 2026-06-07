4bce7bd31a76296254f4b7a71c0019ed17ef842d
pweigert64 <pweigert64@gmail.com>
2026-06-07 15:22:50 +0200

    Zoon 12 for direct Link

--- app.js
+++ app.js
@@ -702,7 +702,7 @@ function jumpToMap(indices) {
             // Genau ein Marker gefunden
             const p = uniqueMarkers[0];
             const originalIdx = photoMarkers.indexOf(p);
-            map.flyTo([p.lat, p.lon], 10, { duration: 1.5 });
+            map.flyTo([p.lat, p.lon], 12, { duration: 1.5 });
 
             // Popup nur bei Einzelziel öffnen
             map.once('moveend', () => {
